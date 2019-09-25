68a449ba9a67c77adbad54cc5ce11a224fe043ad
Geoff Rich <geoff.rich@gmail.com>
2019-09-25 11:29:29 +1000

    Capture all fields.

--- initializeData.js
+++ initializeData.js
@@ -7,11 +7,35 @@ function requestAndParsePromise(url) {
             var parsedJavascript = JSON.parse(body);
             let returnArray = [];
             parsedJavascript.prices.forEach((priceRecord) => {
-                returnArray.push({
-                  region: priceRecord.attributes["aws:region"],
-                  operatingSystem: priceRecord.attributes["aws:ec2:operatingSystem"],
-                  instanceType: priceRecord.attributes["aws:ec2:instanceType"]
-                });
+              let price = parseFloat(priceRecord.price.USD) * 100000;
+              let priceInt = Math.floor(price);
+              returnArray.push({
+                region: priceRecord.attributes["aws:region"],
+                capacitystatus : priceRecord.attributes["aws:ec2:capacitystatus"],
+                clockSpeed:  priceRecord.attributes["aws:ec2:clockSpeed"],
+                currentGeneration : priceRecord.attributes["aws:ec2:currentGeneration"],
+                dedicatedEbsThroughput: priceRecord.attributes["aws:ec2:dedicatedEbsThroughput"],
+                ecu : priceRecord.attributes["aws:ec2:ecu"],
+                enhancedNetworkingSupported: priceRecord.attributes["aws:ec2:enhancedNetworkingSupported"],
+                instanceFamily : priceRecord.attributes["aws:ec2:instanceFamily"],
+                instanceType: priceRecord.attributes["aws:ec2:instanceType"],
+                licenseModel: priceRecord.attributes["aws:ec2:licenseModel"],
+                memory: priceRecord.attributes["aws:ec2:memory"],
+                networkPerformance: priceRecord.attributes["aws:ec2:networkPerformance"],
+                normalizationSizeFactor: priceRecord.attributes["aws:ec2:normalizationSizeFactor"],
+                operatingSystem: priceRecord.attributes["aws:ec2:operatingSystem"],
+                operation: priceRecord.attributes["aws:ec2:operation"],
+                physicalProcessor: priceRecord.attributes["aws:ec2:physicalProcessor"],
+                preInstalledSw: priceRecord.attributes["aws:ec2:preInstalledSw"],
+                processorArchitecture: priceRecord.attributes["aws:ec2:processorArchitecture"],
+                processorFeatures: priceRecord.attributes["aws:ec2:processorFeatures"],
+                storage: priceRecord.attributes["aws:ec2:storage"],
+                tenancy: priceRecord.attributes["aws:ec2:tenancy"],
+                term: priceRecord.attributes["aws:ec2:term"],
+                usagetype: priceRecord.attributes["aws:ec2:usagetype"],
+                vcpu: parseInt(priceRecord.attributes["aws:ec2:vcpu"]),
+                price: priceInt
+              });
             }, this);
             resolve(returnArray);
         });
--- models/instance.js
+++ models/instance.js
@@ -4,7 +4,7 @@ module.exports = function(sequelize, DataTypes) {
       clockSpeed: DataTypes.STRING,
       currentGeneration: DataTypes.STRING,
       dedicatedEbsThroughput: DataTypes.STRING,
-      ecu: DataTypes.INTEGER,
+      ecu: DataTypes.STRING,
       enhancedNetworkingSupported: DataTypes.STRING,
       instanceFamily: DataTypes.STRING,
       instanceType: DataTypes.STRING,
@@ -22,8 +22,9 @@ module.exports = function(sequelize, DataTypes) {
       tenancy: DataTypes.STRING,
       term: DataTypes.STRING,
       usagetype: DataTypes.STRING,
-      vcpu : DataTypes.STRING,
+      vcpu : DataTypes.INTEGER,
       region : DataTypes.STRING,
+      price: DataTypes.BIGINT
     },
     {
         // Index by region
@@ -37,29 +38,3 @@ module.exports = function(sequelize, DataTypes) {
         }]
     });
 };
-
-/*
-        "aws:ec2:capacitystatus": "Used",
-        "aws:ec2:clockSpeed": "2.9 GHz",
-        "aws:ec2:currentGeneration": "Yes",
-        "aws:ec2:dedicatedEbsThroughput": "2000 Mbps",
-        "aws:ec2:ecu": "62",
-        "aws:ec2:enhancedNetworkingSupported": "Yes",
-        "aws:ec2:instanceFamily": "Compute optimized",
-        "aws:ec2:instanceType": "c4.4xlarge",
-        "aws:ec2:licenseModel": "No License required",
-        "aws:ec2:memory": "30 GiB",
-        "aws:ec2:networkPerformance": "High",
-        "aws:ec2:normalizationSizeFactor": "32",
-        "aws:ec2:operatingSystem": "Linux",
-        "aws:ec2:operation": "RunInstances",
-        "aws:ec2:physicalProcessor": "Intel Xeon E5-2666 v3 (Haswell)",
-        "aws:ec2:preInstalledSw": "NA",
-        "aws:ec2:processorArchitecture": "64-bit",
-        "aws:ec2:processorFeatures": "Intel AVX; Intel AVX2; Intel Turbo",
-        "aws:ec2:storage": "EBS only",
-        "aws:ec2:tenancy": "Shared",
-        "aws:ec2:term": "on-demand",
-        "aws:ec2:usagetype": "APS2-BoxUsage:c4.4xlarge",
-        "aws:ec2:vcpu"
-*/
